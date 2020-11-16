25d74df12bfe7774a98043afc0e867c4c894d210
Crowdbotics <team@crowdbotics.com>
2020-11-16 21:52:00 +0000

    Update models

--- life_changers_22682/wsgi.py
+++ life_changers_22682/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'life_changers_22682.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "life_changers_22682.settings")
 
 application = get_wsgi_application()
